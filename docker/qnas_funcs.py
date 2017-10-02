edf8fc3dadd55e58813a4753ff79d6f114ff29ae
bjohnson <ben@gophronesis.com>
2017-10-02 13:22:10 -0400

    redis work queuing

--- docker/qnas_funcs.py
+++ docker/qnas_funcs.py
@@ -4,8 +4,14 @@
     qnas_funcs.py
 """
 
+import os
 import time
+from datetime import datetime
 
 def run_job(x):
     time.sleep(2)
     return x, x ** 2
+
+def kill():
+    print >> sys.stderr, 'kill signal received at %s' % datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    os._exit(0)
