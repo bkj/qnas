ca39b8b965a204a24ebab9f2ede0703b5699b381
bjohnson <ben@gophronesis.com>
2017-10-02 11:38:27 -0400

    updating download scripts

--- utils/validation-splits.py
+++ utils/validation-splits.py
@@ -36,7 +36,8 @@ for name in glob('./data/*'):
     
     train, val = train_test_split(df, stratify=df.lab, train_size=train_size, random_state=random_state)
     train, val = train.copy(), val.copy()
-    
+    print train.head()
+    print val.head()
     for dest, split in zip(dests, [train, val]):
         split['dest'] = split.apply(lambda x: os.path.join(dest, x['lab'], x['fname']), 1)
         
