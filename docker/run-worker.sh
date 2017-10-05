c9898016022b6c091d5442d77b3e45f2151a2660
bjohnson <bkj.322@gmail.com>
2017-10-05 18:26:51 -0400

    typo

--- docker/run-worker.sh
+++ docker/run-worker.sh
@@ -4,8 +4,11 @@
 #
 # Start worker
 
-cd /root/projects/qnas/docker/
+cd /root/projects/qnas/
 git pull
+python setup.py clean --all install
+
+cd docker
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 rqworker --url redis://:$QNAS_PASSWORD@$QNAS_HOST:$QNAS_PORT kill jobs 2>&1 | tee -a log
