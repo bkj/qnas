9bb2c4861d22b67ef83a9bc35c92debda79bfad8
bjohnson <bkj.322@gmail.com>
2017-10-05 18:21:58 -0400

    typos

--- qnas/controllers/optimizer_search.py
+++ qnas/controllers/optimizer_search.py
@@ -63,7 +63,9 @@ optimizer_space = {
     ]
 }
 
-class OptimizerSampler(object):
+# --
+
+class RandomOptimizerSampler(object):
     def sample(self, depth=0, seed=None):
         if seed:
             random.seed(123)
@@ -81,30 +83,30 @@ class OptimizerSampler(object):
 
 class RandomOptimizerController(object):
     def __init__(self, depth=1, **kwargs):
-        self.depth = depth
-        self.sampler = OptimizerSampler()
-        
-        self.run_name = kwargs['run_name']
-        
-        self.configs_dir = os.path.join(kwargs['outdir'], self.run_name, 'configs')
+        self.configs_dir = os.path.join(kwargs['outdir'], kwargs['run_name'], 'configs')
         if not os.path.exists(self.configs_dir):
             os.makedirs(self.configs_dir)
             
-        self.hists_dir = os.path.join(kwargs['outdir'], self.run_name, 'hists')
+        self.hists_dir = os.path.join(kwargs['outdir'], kwargs['run_name'], 'hists')
         if not os.path.exists(self.hists_dir):
             os.makedirs(self.hists_dir)
+        
+        self.run_name = kwargs['run_name']
+        
+        self.depth = depth
+        self.random_sampler = RandomOptimizerSampler()
     
     def _next(self, last=None):
         return {
             "func" : "qnas_trainer",
             "config" : {
-                "model_name"  : "%s-%s" % (args.run_name, str(uuid4())),
+                "model_name"  : "%s-%s" % (self.run_name, str(uuid4())),
                 "net_class"   : 'OptNetSmall',
                 "dataset"     : 'CIFAR10',
                 "epochs"      : 1,
                 "lr_schedule" : 'constant',
                 "lr_init"     : 0.01,
-                "opt_arch"    : self.sampler.sample(depth=self.depth),
+                "opt_arch"    : self.random_sampler.sample(depth=self.depth),
             },
             "cuda" : True,
             "lr_fail_factor" : 0.5,
@@ -113,6 +115,75 @@ class RandomOptimizerController(object):
     
     def seed(self):
         return self._next()
+    
+    def success(self, last):
+        config, hist = last
+        print('job finished: %s' % json.dumps(config), file=sys.stderr)
+        
+        # Save results
+        open(os.path.join(self.configs_dir, config['model_name']), 'w').write(json.dumps(config))
+        open(os.path.join(self.hists_dir, config['model_name']), 'w').write('\n'.join(map(json.dumps, hist)))
+        
+        return self._next()
+    
+    def failure(self, last):
+        print('job failed!', file=sys.stderr)
+        return self._next()
+
+# --
+
+class PPOOptimizerSampler(object):
+    def __init__(self, depth=0):
+        self.depth = 0
+    
+    def update(self, arch, reward):
+        
+        # --
+        # Update model
+        # self.net.update(arch, reward)
+        
+    def sample(self):
+        
+        # --
+        # Sample from policy
+        # return self.net.sample()
+
+
+class PPOOptimizerController(object):
+    def __init__(self, depth=1, **kwargs):
+        self.configs_dir = os.path.join(kwargs['outdir'], kwargs['run_name'], 'configs')
+        if not os.path.exists(self.configs_dir):
+            os.makedirs(self.configs_dir)
+            
+        self.hists_dir = os.path.join(kwargs['outdir'], kwargs['run_name'], 'hists')
+        if not os.path.exists(self.hists_dir):
+            os.makedirs(self.hists_dir)
+        
+        self.run_name = kwargs['run_name']
+        
+        self.depth = depth
+        self.ppo_sampler = PPOOptimizerSampler()
+        self.random_sampler = RandomOptimizerSampler(self.depth)
+        
+    def _wrap(self, arch):
+        return {
+            "func" : "qnas_trainer",
+            "config" : {
+                "model_name"  : "%s-%s" % (self.run_name, str(uuid4())),
+                "net_class"   : 'OptNetSmall',
+                "dataset"     : 'CIFAR10',
+                "epochs"      : 1,
+                "lr_schedule" : 'constant',
+                "lr_init"     : 0.01,
+                "opt_arch"    : arch,
+            },
+            "cuda" : True,
+            "lr_fail_factor" : 0.5,
+            "dataset_num_workers" : 8
+        }
+    
+    def seed(self):
+        return self._wrap(arch=self.random_sampler(depth=self.depth))
     
     def success(self, last):
         config, hist = last
