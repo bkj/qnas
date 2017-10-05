3e9f1fb5693f310a6d6154d44f60996fce5587ab
Ben Johnson <bkj.322@gmail.com>
2017-10-05 19:54:38 -0400

    Update provision-redis-ec2.sh

--- docker/ec2/provision-redis-ec2.sh
+++ docker/ec2/provision-redis-ec2.sh
@@ -4,13 +4,17 @@
 
 sudo apt-get update
 sudo add-apt-repository -y ppa:chris-lea/redis-server
+
 sudo apt-get update
 sudo apt-get install -y redis-server
 
 redis-cli shutdown
 
 sudo vi  /etc/redis/redis.conf
-# 1 - Change port => port 6879
-# 2 - Add password => requirepass XXXXX
-# 3 - Change bind => comment out `# bind 127.0.0.1`
+# 1 - Change port => port XXXX
+# 2 - Add password => requirepass YYYYYYYYYYYYYYYYY
+# 3 - Change bind to open up => comment out `# bind 127.0.0.1`
 
+redis-cli start
+
+# ... or something ... those `redis` commands might be incorrect
