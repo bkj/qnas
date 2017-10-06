c4078216d7c0a91a4cdb65e1831a5b72a9e3670c
bjohnson <bkj.322@gmail.com>
2017-10-06 12:38:43 -0400

    more shared memory

--- docker/launch.sh
+++ docker/launch.sh
@@ -8,7 +8,7 @@
 # Run a GPU worker
 
 source credentials.sh
-sudo NV_GPU=$1 nvidia-docker run -it \
+sudo NV_GPU=$1 nvidia-docker run --shm-size=1g -it \
     -e QNAS_HOST=$QNAS_HOST \
     -e QNAS_PORT=$QNAS_PORT \
     -e QNAS_PASSWORD=$QNAS_PASSWORD \
